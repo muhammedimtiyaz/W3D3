049473c777aa1c8c10f54034ab7d240d47dbbfc0
App Academy Student <muhammedimtiyaz87@gmail.com>
2018-12-12 16:26:20 -0500

    Update finish association exercise

--- Activerecord_Association/app/models/course.rb
+++ Activerecord_Association/app/models/course.rb
@@ -1,6 +1,18 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint(8)        not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Course < ApplicationRecord
 
-    belongs_to :prereqs,
+    belongs_to :prereq,
         class_name: 'Course',
         primary_key: :id,
         foreign_key: :prereq_id
@@ -19,13 +31,8 @@ class Course < ApplicationRecord
         through: :enrollments,
         source: :user     
     
-    has_many :courses,
+    has_many :postreqs,
         class_name: 'Course',
         primary_key: :id,
         foreign_key: :prereq_id
-
-    has_many :users,
-        through: :enrollments,
-        source: :user
-
 end
--- Activerecord_Association/app/models/user.rb
+++ Activerecord_Association/app/models/user.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
     has_many :enrollments,
         class_name: 'Enrollment',
@@ -7,5 +17,10 @@ class User < ApplicationRecord
     has_many :enrolled_courses,
         through: :enrollments,
         source: :course
+
+    has_many :taught_courses,
+        class_name: "Course",
+        primary_key: :id,
+        foreign_key: :instructor_id
 end
  
